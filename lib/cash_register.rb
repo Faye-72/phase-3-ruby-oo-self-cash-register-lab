22d7b94ac55ff49aa7e1ea417cfa91470e688f7a
fmitei <faith.mitei@trustgro.com>
2023-04-02 18:08:24 +0300

    completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
 
+class CashRegister
+    
+     attr_accessor :discount, :total ,:title , :price, :items ,:last_transaction_amount 
+
+    def initialize(discount = 0)
+     @discount = discount
+     @total = 0
+     @items = []
+    end
+
+
+    def add_item (title,price,quantity = 1)
+       @total += price * quantity
+       quantity.times do
+        items << title
+       end
+       self.last_transaction_amount = price * quantity
+    end
+
+    def apply_discount
+        if @discount > 0 
+           self.total =self.total - ((self.total * self.discount)/100)
+            "After the discount, the total comes to $#{self.total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+     @total = self.total - self.last_transaction_amount 
+    end
+    
+
+end
+
+discount = CashRegister.new(20)
